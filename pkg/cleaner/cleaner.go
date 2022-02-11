079cf856b41fd69fb6d4a6a612f73e83675243e5
Maxim Pogozhiy <foxdalas@gmail.com>
2022-02-11 18:38:37 +0700

    first commit

--- pkg/cleaner/cleaner.go
+++ pkg/cleaner/cleaner.go
@@ -1,12 +1,15 @@
 package cleaner
 
-import "C"
 import (
 	"context"
+	"fmt"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 	"github.com/shirou/gopsutil/disk"
+	"github.com/sirupsen/logrus"
+	"syscall"
+	"time"
 )
 
 type DockerDiskUsage struct {
@@ -15,16 +18,51 @@ type DockerDiskUsage struct {
 	Volumes    float64
 }
 
+type Usage struct {
+	DiskUsageGb       float64
+	DiskUsagePercents float64
+	BuildCacheUsage   float64
+	ContainerUsage    float64
+	VolumesUsage      float64
+}
+
 type Cleaner struct {
 	Docker *client.Client
 	Ctx    context.Context
+	Log    *logrus.Entry
+	Dir    string
+	TTL    time.Duration
 }
 
-func New(client *client.Client, ctx context.Context) *Cleaner {
+func New(client *client.Client, ctx context.Context, log *logrus.Entry, dir string, ttl time.Duration) *Cleaner {
 	return &Cleaner{
 		Docker: client,
 		Ctx:    ctx,
+		Log:    log,
+		Dir:    dir,
+		TTL:    ttl,
 	}
+}
+
+func (Cleaner *Cleaner) GetUsageInfo() (*Usage, error) {
+	diskSpaceUsageGB, diskSpaceUsagePercents, err := Cleaner.DeviceSpaceUsage(Cleaner.Dir)
+	if err != nil {
+		return nil, fmt.Errorf("device %s: %s", Cleaner.Dir, err)
+	}
+
+	dockerSpaceUsage, err := Cleaner.DockerDiskUsage()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Usage{
+		DiskUsageGb:       diskSpaceUsageGB,
+		DiskUsagePercents: diskSpaceUsagePercents,
+		BuildCacheUsage:   dockerSpaceUsage.BuildCache,
+		ContainerUsage:    dockerSpaceUsage.Containers,
+		VolumesUsage:      dockerSpaceUsage.Volumes,
+	}, nil
+
 }
 
 // GB, Percent, Error
@@ -34,6 +72,19 @@ func (Cleaner *Cleaner) DeviceSpaceUsage(device string) (float64, float64, error
 		return 0, 0, err
 	}
 	return float64(usage.Used) / (1 << 30), float64(usage.UsedPercent), err
+}
+
+func (Cleaner *Cleaner) GetDiskUtilization(path string) (float64, float64, error) {
+	var stat syscall.Statfs_t
+	err := syscall.Statfs(path, &stat)
+	if err != nil {
+		return 0, 0, err
+	}
+	all := stat.Blocks * uint64(stat.Bsize)
+	free := stat.Bfree * uint64(stat.Bsize)
+	used := all - free
+	percentageUtilized := float64(used) / float64(all) * float64(100)
+	return float64(used) / (1 << 30), float64(percentageUtilized), err
 }
 
 func (Cleaner *Cleaner) DockerDiskUsage() (*DockerDiskUsage, error) {
@@ -64,8 +115,13 @@ func (Cleaner *Cleaner) DockerDiskUsage() (*DockerDiskUsage, error) {
 }
 
 func (Cleaner *Cleaner) BuildCachePrune() (float64, error) {
+	filter := filters.NewArgs(filters.KeyValuePair{
+		Key:   "until",
+		Value: Cleaner.TTL.String(),
+	})
 	opts := types.BuildCachePruneOptions{
-		All: true,
+		All:     true,
+		Filters: filter,
 	}
 
 	report, err := Cleaner.Docker.BuildCachePrune(Cleaner.Ctx, opts)
@@ -78,7 +134,7 @@ func (Cleaner *Cleaner) BuildCachePrune() (float64, error) {
 func (Cleaner *Cleaner) ContainersPrune() (float64, error) {
 	filter := filters.NewArgs(filters.KeyValuePair{
 		Key:   "until",
-		Value: "48h",
+		Value: Cleaner.TTL.String(),
 	})
 
 	report, err := Cleaner.Docker.ContainersPrune(Cleaner.Ctx, filter)

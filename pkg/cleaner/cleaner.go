f48eb0ccb007093b5b7b20ffd200fd3377965946
Maxim Pogozhiy <foxdalas@gmail.com>
2022-02-11 18:23:58 +0700

    first commit

---
+++ pkg/cleaner/cleaner.go
@@ -0,0 +1,99 @@
+package cleaner
+
+import "C"
+import (
+	"context"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/client"
+	"github.com/shirou/gopsutil/disk"
+)
+
+type DockerDiskUsage struct {
+	BuildCache float64
+	Containers float64
+	Volumes    float64
+}
+
+type Cleaner struct {
+	Docker *client.Client
+	Ctx    context.Context
+}
+
+func New(client *client.Client, ctx context.Context) *Cleaner {
+	return &Cleaner{
+		Docker: client,
+		Ctx:    ctx,
+	}
+}
+
+// GB, Percent, Error
+func (Cleaner *Cleaner) DeviceSpaceUsage(device string) (float64, float64, error) {
+	usage, err := disk.Usage(device)
+	if err != nil {
+		return 0, 0, err
+	}
+	return float64(usage.Used) / (1 << 30), float64(usage.UsedPercent), err
+}
+
+func (Cleaner *Cleaner) DockerDiskUsage() (*DockerDiskUsage, error) {
+	var buildCacheUsage float64
+	var containersUsage float64
+	var volumesUsage float64
+
+	diskUsage, err := Cleaner.Docker.DiskUsage(Cleaner.Ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, buildCache := range diskUsage.BuildCache {
+		buildCacheUsage += float64(buildCache.Size)
+	}
+	for _, container := range diskUsage.Containers {
+		containersUsage += float64(container.SizeRootFs)
+	}
+	for _, volume := range diskUsage.Volumes {
+		volumesUsage += float64(volume.UsageData.Size)
+	}
+
+	return &DockerDiskUsage{
+		BuildCache: buildCacheUsage / (1 << 30),
+		Containers: containersUsage / (1 << 30),
+		Volumes:    volumesUsage / (1 << 30),
+	}, err
+}
+
+func (Cleaner *Cleaner) BuildCachePrune() (float64, error) {
+	opts := types.BuildCachePruneOptions{
+		All: true,
+	}
+
+	report, err := Cleaner.Docker.BuildCachePrune(Cleaner.Ctx, opts)
+	if err != nil {
+		return 0, err
+	}
+	return float64(report.SpaceReclaimed) / (1 << 30), nil
+}
+
+func (Cleaner *Cleaner) ContainersPrune() (float64, error) {
+	filter := filters.NewArgs(filters.KeyValuePair{
+		Key:   "until",
+		Value: "48h",
+	})
+
+	report, err := Cleaner.Docker.ContainersPrune(Cleaner.Ctx, filter)
+	if err != nil {
+		return 0, err
+	}
+	return float64(report.SpaceReclaimed) / (1 << 30), nil
+}
+
+func (Cleaner *Cleaner) VolumesPrune() (float64, error) {
+	filter := filters.NewArgs()
+
+	report, err := Cleaner.Docker.VolumesPrune(Cleaner.Ctx, filter)
+	if err != nil {
+		return 0, err
+	}
+	return float64(report.SpaceReclaimed) / (1 << 30), nil
+}
